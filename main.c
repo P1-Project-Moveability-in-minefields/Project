d4f98e0bc43d5ddb08d7dcfe500e9b6a48229faf
Lukas Saltenis <lsaltenis@hotmail.com>
2023-12-04 20:06:07 +0100

    test of dijkstra added

--- main.c
+++ main.c
@@ -5,12 +5,31 @@
 
 
 int main() {
+    // test af dijkstra
+    int size = 4;
+    int *input[size];
+    int row0[] = {1,3,11,4};
+    int row1[] = {7,60,40,6};
+    int row2[] = {6,27,50,1};
+    int row3[] = {30,6,7,5};
+    input[0] = row0;
+    input[1] = row1;
+    input[2] = row2;
+    input[3] = row3;
+
+    int start_pos[2] = {3,0};
+    int end_pos[2] = {3,3};
+
+    result *optimal_path = dijkstra(input,size, start_pos,end_pos);
+
+    for (int i = optimal_path->path_length-1; i >= 0; --i) {
+        printf("(%d,%d)->",optimal_path->path[i][0], optimal_path->path[i][1]);
+    }
+    printf("Goal \n");
+    printf("Path length = %d", optimal_path->path_length);
+
 
     obtain_user_settings();
-
-
-
-
 
     // En matrix (3x3) som et eksempel, bare et for at teste jeg satte den her ind.
     int myMatrix[ROWS][COLS] = {
