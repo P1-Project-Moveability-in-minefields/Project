71558e7aac411d4090a62b0b0507976c14f7997c
JesperDA <JesperDA@hotmail.com>
2023-12-05 13:23:44 +0100

    Configurematrices, mine, soil, vegitation, road.

--- Relationship Methods/ConfigureMatrices.c
+++ Relationship Methods/ConfigureMatrices.c
@@ -24,7 +24,7 @@ MatrixInfo ProcessWaterMapRelationships(MatrixInfo waterMap, userSettings settin
 
     for (int i = 0; i < waterMap.rows; ++i) {
         for (int j = 0; j < waterMap.cols; ++j) {
-            if (/*waterMap.matrix[i][j] > settings.acceptableWaterlevel*/ true){
+            if (/*waterMap.matrix[i][j] > settings.acceptableWater*/ true){
              waterMap.matrix[i][j] = -1;
             }
         }
@@ -34,16 +34,52 @@ MatrixInfo ProcessWaterMapRelationships(MatrixInfo waterMap, userSettings settin
 
 MatrixInfo ConfigureMineMap(MatrixInfo mineMap, userSettings settings){
 
+    for (int i = 0; i < mineMap.rows; ++i) {
+        for (int j = 0; j < mineMap.cols; ++j) {
+            if (/*mineMap.matrix[i][j] > settings.acceptableMine*/ true){
+                mineMap.matrix[i][j] = -1;
+            }
+        }
+    }
+    return mineMap;
+
 }
 
 MatrixInfo ConfigureSoilMap(MatrixInfo soilMap, userSettings settings){
+
+    for (int i = 0; i < soilMap.rows; ++i) {
+        for (int j = 0; j < soilMap.cols; ++j) {
+            if (/*soilMap.matrix[i][j] > settings.acceptableSoil*/ true){
+                soilMap.matrix[i][j] = -1;
+            }
+        }
+    }
+    return soilMap;
 
 }
 
 MatrixInfo ConfigureVegitationMap(MatrixInfo vegitationMap, userSettings settings){
 
+    for (int i = 0; i < vegitationMap.rows; ++i) {
+        for (int j = 0; j < vegitationMap.cols; ++j) {
+            if (/*vegitationMap.matrix[i][j] > settings.acceptableVegitation*/ true){
+                vegitationMap.matrix[i][j] = -1;
+            }
+        }
+    }
+    return vegitationMap;
+
 }
 
 MatrixInfo ConfigureRoadMap(MatrixInfo roadMap, userSettings settings){
-    
+
+    for (int i = 0; i < roadMap.rows; ++i) {
+        for (int j = 0; j < roadMap.cols; ++j) {
+            if (/*roadMap.matrix[i][j] > settings.acceptableRoad*/ true){
+                roadMap.matrix[i][j] = -1;
+            }
+        }
+    }
+    return roadMap;
+
 }
