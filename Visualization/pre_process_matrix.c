a1cd50cf0836680a68e62c432c0c922a2c4d5aa9
lynneruup <andreaslynnerup@outlook.dk>
2023-12-06 12:43:14 +0100

    foundation for pre processing

--- Visualization/pre_process_matrix.c
+++ Visualization/pre_process_matrix.c
@@ -1,10 +1,29 @@
 //
 // Created by Andreas Lynnerup on 06/12/2023.
 ///
-
+#define NUMBEROFMATRICES 5
 #include "pre_process_matrix.h"
 
-double** createMatrixPainting(list)
+double** createMatrixPainting(int rows, int cols,
+                              double** soilMatrix[rows][cols],
+                              double** waterMatrix[rows][cols],
+                              double** vegetationMatrix[rows][cols],
+                              double** roadMatrix[rows][cols],
+                              double** mineMatrix[rows][cols],
+                              int** optimal_route){
+
+    double** matrixPainting[rows][cols];
+
+    addSoilToMatrix(matrixPainting, soilMatrix);
+    addWaterToMatrix(matrixPainting);
+    addVegetationToMatrix(matrixPainting);
+    addRoadToMatrix(matrixPainting);
+    addMineToMatrix(matrixPainting);
+
+
 }
 
-void
+
+void addSoilToMatrix(double** painting) {
+
+}
