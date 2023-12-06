1c0a053360356ee8c50e3a4b79d1738131b42e40
Lukas Saltenis <tl00xu@student.aau.dk>
2023-12-06 10:33:35 +0100

    matrix_info struct deleted

--- Configuration Of Matrices/ConfigureMatrices.h
+++ Configuration Of Matrices/ConfigureMatrices.h
@@ -7,24 +7,17 @@
 #ifndef P1_PROJECT_CONFIGUREMATRICES_H
 #define P1_PROJECT_CONFIGUREMATRICES_H
 
-
-typedef struct {
-    double** matrix;
-    int rows;
-    int cols;
-} MatrixInfo;
-
 typedef struct{
-    MatrixInfo matrixInfo;
+    double** matrix;
     double weight;
 } WeightedMatrix;
 
-void ConfigureSlopeMap(MatrixInfo slopeMap, userSettings settings);
-void ConfigureWaterMap(MatrixInfo waterMap, userSettings settings);
-void ConfigureMineMap(MatrixInfo mineMap, userSettings settings);
-void ConfigureSoilMap(MatrixInfo soilMap, userSettings settings);
-void ConfigureVegitationMap(MatrixInfo vegitationMap, userSettings settings);
-void ConfigureRoadQualityMap(MatrixInfo roadQualityMap, userSettings settings);
+void ConfigureSlopeMap(WeightedMatrix slopeMap, userSettings settings);
+void ConfigureWaterMap(WeightedMatrix waterMap, userSettings settings);
+void ConfigureMineMap(WeightedMatrix mineMap, userSettings settings);
+void ConfigureSoilMap(WeightedMatrix soilMap, userSettings settings);
+void ConfigureVegitationMap(WeightedMatrix vegitationMap, userSettings settings);
+void ConfigureRoadQualityMap(WeightedMatrix roadQualityMap, userSettings settings);
 
 
 #endif //P1_PROJECT_CONFIGUREMATRICES_H
