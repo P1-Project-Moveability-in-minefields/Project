e9401ce12f651dc65e1c4c9a3faf06334732d282
Lukas Saltenis <tl00xu@student.aau.dk>
2023-12-06 15:23:06 +0100

    function documentation added

--- Dijkstra's algorithm/dijkstra.h
+++ Dijkstra's algorithm/dijkstra.h
@@ -8,7 +8,22 @@ typedef struct{
     int path_length; // number of nodes in path
 }result;
 
+/// Finds the optimal path based on user_settings and a weighted sum of the input data
+///
+/// Parameters:
+/// \param input (double **) is a 2D-array containing the "cost" of every grid_cell of a geographical area.
+/// \param size (int) is the dimension of the 2D-array.
+/// \param start_pos (int [2]) array containing the coordinates of the starting position
+/// \param end_pos (int [2]) array containing the coordinates of the starting position
+/// \return returns a pointer to a result struct, which contains the path and its length
 result *dijkstra(double **input, int size, const int start_pos[], const int end_pos[]);
+
+/// Backtracks the path found by dijktra()
+///
+/// Parameters:
+/// \param goal (entry *) is the end node. Every node stores the previous node in the path.
+/// \param size (int) is the dimension of the 2D-array, that represents the graph.
+/// \return returns a pointer to a result struct, which contains the path and its length
 result *backtrace_path(entry *goal, int size);
 
 #endif //P1_PROJECT_DIJKSTRA_H
