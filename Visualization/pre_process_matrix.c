2c9b3e27f8b6083d3e7d22b2d2469a75ad96fa0f
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2023-12-06 12:59:23 +0100

    Update pre_process_matrix.c
    Did some function work and added an include

--- Visualization/pre_process_matrix.c
+++ Visualization/pre_process_matrix.c
@@ -3,6 +3,7 @@
 ///
 #define NUMBEROFMATRICES 5
 #include "pre_process_matrix.h"
+#include "stdio.h"
 
 double** createMatrixPainting(int rows, int cols,
                               double** soilMatrix[rows][cols],
@@ -28,7 +29,20 @@ void addSoilToMatrix(double** matrixPainting, double** terrainMatrix){
 
 }
 void addWaterToMatrix(int size, double** matrix, double** terrainMatrix){
+    if (terrainMatrix == NULL)
+    for (int row = 0; row < size; ++row) {
+        for (int col = 0; col < size; ++col) {
+            double value = terrainMatrix[row][col];
+            if (value > 0){
+                if (value > 0.5){
 
+                }
+                else {
+
+                }
+            }
+        }
+    }
 }
 void addVegetationToMatrix(double** matrix, double** terrainMatrix){
 
