c9cf9425fa7f4273d217e4748ab6b48da51b1cbd
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2023-12-06 12:51:49 +0100

    change function signature again
    no rows or cols, now size

--- Visualization/pre_process_matrix.h
+++ Visualization/pre_process_matrix.h
@@ -6,9 +6,9 @@
 #define P1_PROJECT_PRE_PROCESS_MATRIX_H
 
 void addSoilToMatrix(double** matrixPainting, double** terrainMatrix);
-void addWaterToMatrix(int rows, int cols, double** matrix, double** terrainMatrix);
+void addWaterToMatrix(int size, double** matrix, double** terrainMatrix);
 void addVegetationToMatrix(double** matrix, double** terrainMatrix);
-void addRoadToMatrix(int rows, int cols, double** matrix, double** terrainMatrix);
-void addMineToMatrix(int rows, int cols, double** matrix, double** terrainMatrix);
+void addRoadToMatrix(int size, double** matrix, double** terrainMatrix);
+void addMineToMatrix(int size, double** matrix, double** terrainMatrix);
 
 #endif //P1_PROJECT_PRE_PROCESS_MATRIX_H
