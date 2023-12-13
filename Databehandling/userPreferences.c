3efa96d69d6bca2ff186ef26ea605a10d9c2ed52
Lukas Saltenis <tl00xu@student.aau.dk>
2023-12-13 19:10:27 +0100

    settings updated

--- Databehandling/userPreferences.c
+++ Databehandling/userPreferences.c
@@ -19,14 +19,14 @@ userSettings obtain_user_settings() {
     userSettings chosenSettings;
 
     printf("\nPlease decide setup parameters");
-    printf("\nDecide the following 4 settings:\n");
+    printf("\nDecide the following 3 settings:\n");
     printf("\n1. Which troop type should maneuver?");
     printf("\n2. How much risk is acceptable?");
     /*printf("\n3. Priority of roads");
     printf("\n4. Priority of terrain");
-    printf("\n5. Should cover be prioritized?");*/
-    printf("\n6. Which kinds of clearance implements are desired for usage?");
-    printf("\n7. Size of matrices?\n");
+    printf("\n5. Should cover be prioritized?");
+    printf("\n6. Which kinds of clearance implements are desired for usage?");*/
+    printf("\n3. Size of matrices?\n");
 
     //1st setting
     printf("\n1. Which troop type should maneuver? ('0' = Squad  |  '1' = Company  |  '2' = Logistic unit  |  '3' = Armor unit )\n");
@@ -54,14 +54,14 @@ userSettings obtain_user_settings() {
     printf("\n5. Should cover be prioritized? ('1' = yes  |  '0' = no )\n");
     scanf("%d", &chosenSettings.additional_settings.is_cover_prioritized);
     printf("You have chosen to make cover prioritization %s\n", stringPrioritizeCover[chosenSettings.additional_settings.is_cover_prioritized]);
-    */
+
 
     //6th setting
     printf("\n6. Specify available and desired clearance method: ('0' = None '1' = MICLIC, '2' = Vehicle mounted plow, '3' = Rafael Missile Carpet System\n");
     scanf("%d",&chosenSettings.additionalSettings.clearingImplement);
     printf("You have specified the availability of clearance method %s\n", stringClearanceType[chosenSettings.additionalSettings.clearingImplement]);
-
-    printf("\n7. What is the size of the input matrices?\n");
+    */
+    printf("\n3. What is the size of the input matrices?\n");
     scanf("%d",&chosenSettings.additionalSettings.size);
 
     printf("\nYour chosen settings are: \n");
@@ -70,7 +70,7 @@ userSettings obtain_user_settings() {
     // printf("3. Road priority:       %s\n", stringRiskLevel[chosenSettings.additional_settings.is_cover_prioritized]);
     // printf("4. Terrain priority:    %s\n", stringRiskLevel[chosenSettings.weights.vegetation_weight]);
     // printf("5. Prioritize cover:    %s\n", stringPrioritizeCover[chosenSettings.additional_settings.is_cover_prioritized]);
-    printf("6. Clearance method:    %s\n", stringClearanceType[chosenSettings.additionalSettings.clearingImplement]);
+    // printf("6. Clearance method:    %s\n", stringClearanceType[chosenSettings.additionalSettings.clearingImplement]);
     printf("7. Size of matrices:    %dx%d\n", chosenSettings.additionalSettings.size,chosenSettings.additionalSettings.size);
     return (chosenSettings);
 }
