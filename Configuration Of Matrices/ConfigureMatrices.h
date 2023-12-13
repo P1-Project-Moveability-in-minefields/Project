a080317bc488a186c977ddf974496d90a9480ffb
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2023-12-13 09:09:35 +0100

    Update ConfigureMatrices.h
    Documented a function that was missing

--- Configuration Of Matrices/ConfigureMatrices.h
+++ Configuration Of Matrices/ConfigureMatrices.h
@@ -7,14 +7,17 @@
 #ifndef P1_PROJECT_CONFIGUREMATRICES_H
 #define P1_PROJECT_CONFIGUREMATRICES_H
 
+/// What type is the matrix? (mine, road, soil, steepness, vegetation, water)
 typedef enum {mine, road, soil, steepness, vegetation, water} matrix_type;
 
+/// A data container, with a matrix, the matrix type and its weight
 typedef struct{
     matrix_type type;
     double** matrix;
     double weight;
 } weighted_matrix;
 
+/// A data container,
 typedef struct {
     double altitude;
     double northward_slope;
@@ -29,7 +32,6 @@ typedef struct {
 /// \param slopeMap pointer to 2D array to be filled by function
 /// \param heightMap pointer to 2D array containing altitude values
 /// \param settings pointer to struct array containing global user settings
-/// \return returns a 1 on success
 void create_slope_map(slope_struct** slopeMap, double** heightMap, userSettings *settings);
 
 /// Pre-analysis configuration of watermap to account for user parameters
@@ -37,7 +39,6 @@ void create_slope_map(slope_struct** slopeMap, double** heightMap, userSettings 
 /// Parameters:
 /// \param waterMap (struct) is a matrix pointer and dimension values.
 /// \param settings (struct) is only used for the passable value in question with the map.
-/// \return returns a 1 on success
 void configure_depth_map(double** waterMap, double** road_map, userSettings *settings);
 
 /// Checks for unacceptable mine chances
@@ -53,7 +54,6 @@ void configure_mine_map(double** mineMap, userSettings *settings);
 /// \param mineMaps Array containing pointers to all relevant mine arrays
 /// \param map_count Amount of maps, for iteration
 /// \param settings Pointer to struct with global user settings
-/// \return Returns 1 on success
 void combine_mine_maps(double** mineMap, double*** mineMaps, int map_count, userSettings *settings);
 
 /// Checks for acceptable amount of vegitation
@@ -82,7 +82,10 @@ void configure_road_quality_map(double** roadQualityMap, userSettings *settings)
 /// \param settings pointer to settings struct
 void configure_enemy_activity_map(double** enemyActivityMap, userSettings *settings);
 
-
+/// Parent function to configure all matrices assuming they were formed using the appropriate function.
+/// \param list_of_matrices (double***), the list of matrices.
+/// \param settings (struct*), a reference to access the settings.
+/// \return (struct*), a reference to the configured matrices.
 weighted_matrix* configure_list_of_matrices(double*** list_of_matrices, userSettings *settings);
 
 
