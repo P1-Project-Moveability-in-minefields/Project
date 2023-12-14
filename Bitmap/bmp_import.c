a1eca41d8e8b1d60aa76ae4b4cd7451bdda503bc
PayThePro <paythepro@gmail.com>
2023-12-14 10:44:16 +0100

    bmp_import polish

--- Bitmap/bmp_import.c
+++ Bitmap/bmp_import.c
@@ -4,15 +4,15 @@
 double** import_bmp(char* path) {
     //Opens the file, with read binary parameter
     FILE* filePointer = fopen(path, "rb");
-    
+
     //Exit with failure if the file opening failed
     if (filePointer == NULL) {
         exit(EXIT_FAILURE);
     }
-    
+
     //Get the size of the file
     int fileSize = get_file_size(filePointer);
-    
+
     //Make char(size of 1 byte) array to store the file and write the file into the array
     char bmp[fileSize];
     fread(&bmp, fileSize, 1, filePointer);
@@ -22,10 +22,11 @@ double** import_bmp(char* path) {
 
     //Get the pixel data offset from the 10-14 bytes in the bmp and the width, height and bits per pixel from the
     //different locations in the bmp
-    int pixelDataOffset = (int)bmp[10];
-    int width = (int)bmp[18];
-    int height = (int)bmp[22];
-    int bitsPerPixel = (short)bmp[28];
+    int pixelDataOffset = (int)bmp[OFFSET_OF_PIXEL_DATA];
+    int width = (int)bmp[OFFSET_OF_WIDTH];
+    int height = (int)bmp[OFFSET_OF_HEIGHT];
+    int bitsPerPixel = (short)bmp[OFFSET_OF_BITS_PER_PIXEL];
+    int colorTableSize = (int)bmp[OFFSET_OF_COLOR_TABLE_SIZE];
 
     //Uses other function to create a matrix that follows the standard for this project, which will store the 0-1 values
     //given the strength of the color for each pixel in the bitmap
@@ -35,9 +36,9 @@ double** import_bmp(char* path) {
     int rowSize = (int)ceil(((double)(bitsPerPixel * width) / 32)) * 4;
 
     //Get color table
-    unsigned char colorTable[64]; //16 times 4 bytes for red, green, blue and alpha
-    for (int i = 0; i < 64; ++i) {
-        colorTable[i] = bmp[54 + i]; //Byte 54 is the start of color table
+    unsigned char colorTable[colorTableSize]; //16 times 4 bytes for red, green, blue and alpha
+    for (int i = 0; i < colorTableSize; ++i) {
+        colorTable[i] = bmp[OFFSET_OF_COLOR_TABLE + i]; //Byte 54 is the start of color table
     }
 
     //Loop over each pixel
@@ -47,11 +48,11 @@ double** import_bmp(char* path) {
             char indexValue = bmp[pixelDataOffset + i * rowSize + j/2];
 
             //Gets the index of the pixel, so we can map it to the correct color
-            int maskedIndexValue = indexValue & 0x0F;
+            int maskedIndexValue = indexValue & NIBBLE_MASK;
 
             //Calculate normalised value from one of the color chanels of the pixel, given by the index
             //(only one chanel is needed since input is black and white so RGB are all the same)
-            double finalValue = (double)colorTable[maskedIndexValue*4] / 255;
+            double finalValue = (double)colorTable[maskedIndexValue*4] / UCHAR_MAX;
 
             //Save the value in the array
             pixelColorStrengthNormalizedArray[j][i] = finalValue;
@@ -65,22 +66,26 @@ double** import_bmp(char* path) {
 //Checks whether the bitmap is supported or not
 void check_bmp_supported(const char* bmp) {
     //Exit with failure if not a bitmap file
-    if (bmp[0] != 'B' && bmp[1] != 'M') {
+    if ((short)bmp[OFFSET_OF_FILETYPE_VALUE] == SUPPORTED_FILETYPE_VALUE) {
+        printf("\nUnsupported filetype");
         exit(EXIT_FAILURE);
     }
 
     //Exit with failure if bitmap header is not 40 bytes and therefore a bmp we don't support
-    if (bmp[14] != 40) {
+    if (bmp[OFFSET_OF_HEADER_SIZE] != SUPPORTED_HEADER_SIZE) {
+        printf("\nUnsupported header size");
         exit(EXIT_FAILURE);
     }
 
     //Exit with failure if bitmap is not stored with 4 bits per pixel, because we don't support that
-    if ((short)bmp[28] != 4) {
+    if ((short)bmp[OFFSET_OF_BITS_PER_PIXEL] != SUPPORTED_BITS_PER_PIXEL) {
+        printf("\nUnsupported bpp");
         exit(EXIT_FAILURE);
     }
 
     //Exit with failure if bitmap is not uncompressed, because we don't support that
-    if (bmp[30] != 0) {
+    if (bmp[OFFSET_OF_COMPRESSION] != SUPPORTED_COMPRESSION_TYPE) {
+        printf("\nUnsupported compression");
         exit(EXIT_FAILURE);
     }
 }
--- Bitmap/bmp_import.h
+++ Bitmap/bmp_import.h
@@ -3,6 +3,27 @@
 #include "math.h"
 #include "../Configuration Of Matrices/RelationshipMethods.h"
 
+//The offset of values in the bitmap file type
+#define OFFSET_OF_FILETYPE_VALUE 0
+#define OFFSET_OF_PIXEL_DATA 10
+#define OFFSET_OF_HEADER_SIZE 14
+#define OFFSET_OF_WIDTH 18
+#define OFFSET_OF_HEIGHT 22
+#define OFFSET_OF_BITS_PER_PIXEL 28
+#define OFFSET_OF_COMPRESSION 30
+#define OFFSET_OF_COLOR_TABLE_SIZE 46
+#define OFFSET_OF_COLOR_TABLE 54
+
+//Values we support
+#define SUPPORTED_FILETYPE_VALUE 0x424D
+#define SUPPORTED_HEADER_SIZE 40
+#define SUPPORTED_BITS_PER_PIXEL 4
+#define SUPPORTED_COMPRESSION_TYPE 0
+
+//Mask
+#define NIBBLE_MASK 0x0F
+
+//Prototypes
 double** import_bmp(char* path);
 int get_file_size(FILE* file_pointer);
 void check_bmp_supported(const char* bmp);
