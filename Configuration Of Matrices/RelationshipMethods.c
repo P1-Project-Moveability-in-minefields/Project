62717089c50e75257998bbe9a7aab6a59782944e
Lukas Saltenis <tl00xu@student.aau.dk>
2023-12-18 17:35:25 +0100

    code clarification

--- Configuration Of Matrices/RelationshipMethods.c
+++ Configuration Of Matrices/RelationshipMethods.c
@@ -8,38 +8,40 @@
 // Should be corrected to refer to a doctrinal document.
 #define MAXIMUM_SLOPE 70
 
-double **process_matrix(weightedMatrix matrixArray[], int arrayLength, int matrixSize){
+double **process_matrix(weightedMatrix matrix_array[], int array_length, int matrixSize){
     double **processedMatrix = create_dynamic_matrix(matrixSize);
     double **currentMatrix = NULL;
-    for (int i = 0; i < arrayLength; ++i) {
-        currentMatrix = matrixArray[i].matrix;
+    for (int i = 0; i < array_length; ++i) {
+        currentMatrix = matrix_array[i].matrix;
         for (int j = 0; j < matrixSize; ++j) {
             for (int k = 0; k < matrixSize; ++k) {
                 if (processedMatrix[j][k] == -1){
                 } else if (currentMatrix[j][k] == -1){
                     processedMatrix[j][k] = -1;
-                } else processedMatrix[j][k] += matrixArray[i].weight * matrixArray[i].matrix[j][k];
+                } else processedMatrix[j][k] += matrix_array[i].weight * matrix_array[i].matrix[j][k];
             }
         }
     }
     return processedMatrix;
 }
 
-void determine_weights(weightedMatrix matrixArray [], int arrayLength, userSettings *settings){
-    double baseWeight = (double)1/arrayLength;
+void determine_weights(weightedMatrix matrix_array [], int array_length, userSettings *settings){
+    double baseWeight = (double)1/array_length;
 
-    for (int i = 0; i < arrayLength; ++i) {
-        if(matrixArray[i].type == mine) {
-            switch (settings->priorityLevel.mineRisk) {
-                case High:
-                    matrixArray[i].weight = baseWeight;
-                case Medium:
-                    matrixArray[i].weight = 1.5 * baseWeight;
-                case Low:
-                    matrixArray[i].weight = 1.75 * baseWeight;
-            }
-            baseWeight = (1-matrixArray[i].weight)/(arrayLength-1);
-        } else matrixArray[i].weight = baseWeight;
+    if(matrix_array->type == mine) {
+        switch (settings->priorityLevel.mineRisk) {
+            case High:
+                matrix_array->weight = baseWeight;
+            case Medium:
+                matrix_array->weight = 1.5 * baseWeight;
+            case Low:
+                matrix_array->weight = 5 * baseWeight;
+        }
+        baseWeight = (1-matrix_array->weight)/(array_length-1);
+    } else matrix_array->weight = baseWeight;
+
+    for (int i = 1; i < array_length; ++i) {
+        matrix_array[i].weight = baseWeight;
     }
 }
 
